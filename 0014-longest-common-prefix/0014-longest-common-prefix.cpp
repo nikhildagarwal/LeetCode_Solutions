7ae564113998ce391f2fed5c1d0a771d626d3cca
Nikhil Agarwal <81342764+nikhildagarwal@users.noreply.github.com>
2023-10-16 02:57:00 -0400

    Time: 7 ms (16.2%), Space: 9.6 MB (24.53%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int minLength = numeric_limits<int>::max();
+        int index = -1;
+        string prefix = "";
+        int lengthArray = strs.size();
+        if(lengthArray==1){
+            return strs[0];
+        }
+        for(int i = 0;i<lengthArray;i++){
+            if(strs[i].size()<minLength){
+                minLength = strs[i].size();
+                index = i;
+            }
+        }
+        string firstWord = strs[index];
+        cout<<firstWord;
+        int lengthFirstWord = firstWord.size();
+        for(int i = 0;i<lengthFirstWord;i++){
+            char currChar = firstWord.at(i);
+            for(int j = 0;j<lengthArray;j++){
+                if(strs[j].at(i)!=currChar){
+                    return prefix;
+                }
+            }
+            prefix += currChar;
+        }
+        return firstWord;
+    }
+};
