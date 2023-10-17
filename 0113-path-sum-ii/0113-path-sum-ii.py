e357866265e59aa35fdf1e44ca752927a1b80bec
Nikhil Agarwal <81342764+nikhildagarwal@users.noreply.github.com>
2023-10-17 04:56:52 -0400

    Time: 48 ms (60.01%), Space: 17.8 MB (92.05%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.py
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -1,3 +1,9 @@
+class MyNode:
+    def __init__(self, treenode, prev):
+        self.val = treenode
+        self.prev = prev
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,21 +14,22 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         master_list = []
         self.ts = targetSum
-        self.dfs(root,master_list,"",0)
+        self.dfs(root,master_list,None,0)
         return master_list
     
-    def dfs(self, root, master_list, path, sum):
+    def dfs(self, root, master_list, prev, sum):
         if root is None:
             return
-        if path == "":
-            path += str(root.val)
-        else:
-            path += ":"+str(root.val)
+        curr = MyNode(root.val, prev)
         sum += root.val
         if root.right is None and root.left is None:
             if sum == self.ts:
-                string_list = path.split(":")
-                master_list.append([int(x) for x in string_list])
+                row = []
+                head = curr
+                while head is not None:
+                    row.insert(0,head.val)
+                    head = head.prev
+                master_list.append(row)
             return
-        self.dfs(root.right, master_list, path, sum)
-        self.dfs(root.left, master_list, path, sum)
+        self.dfs(root.right, master_list, curr, sum)
+        self.dfs(root.left, master_list, curr, sum)
