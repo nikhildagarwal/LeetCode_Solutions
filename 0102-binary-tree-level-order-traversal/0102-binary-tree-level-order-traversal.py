2459f101582841f8ecf9e76b2a54b20700b85cc4
Nikhil Agarwal <81342764+nikhildagarwal@users.noreply.github.com>
2023-10-18 15:38:35 -0400

    Time: 45 ms (55.76%), Space: 16.9 MB (94.43%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,9 +1,9 @@
-class Node:
+"""class Node:
     def __init__(self, tn, lvl):
         self.tnode = tn
         self.level = lvl
 
-
+"""
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -12,14 +12,14 @@ class Node:
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        queue = [Node(root,0)]
+        queue = [[root,0]]
         ml = []
         curr_row = []
         lvl = 0
         while queue:
             n = queue.pop(0)
-            curr_lvl = n.level
-            curr_node = n.tnode
+            curr_lvl = n[1]
+            curr_node = n[0]
             if curr_node is not None:
                 if curr_lvl == lvl:
                     curr_row.append(curr_node.val)
@@ -27,8 +27,8 @@ class Solution:
                     lvl += 1
                     ml.append(curr_row)
                     curr_row = [curr_node.val]
-                queue.append(Node(curr_node.left,curr_lvl+1))
-                queue.append(Node(curr_node.right,curr_lvl+1))
+                queue.append([curr_node.left,curr_lvl+1])
+                queue.append([curr_node.right,curr_lvl+1])
         if curr_row:
             ml.append(curr_row)
         return ml
