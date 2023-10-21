593d263deef8129587e664825a1680a6494cda04
Nikhil Agarwal <81342764+nikhildagarwal@users.noreply.github.com>
2023-10-21 05:42:30 -0400

    Time: 26 ms (99.01%), Space: 16.3 MB (65.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,9 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        matrix = [[ 1 for _ in range(n)] for _ in range(m)]
+        matrix = []
         if m == 1  or n == 1:
             return 1
-        for i in range(1,m):
-            for j in range(1,n):
-                matrix[i][j] = matrix[i-1][j] + matrix[i][j-1]
+        for i in range(m):
+            row  = []
+            for j in range(n):
+                if i == 0 or j == 0:
+                    row.append(1)
+                else:
+                    row.append(matrix[i-1][j] + row[j-1])
+            matrix.append(row)
         return matrix[m-1][n-1]
