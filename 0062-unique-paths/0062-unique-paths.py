821f79b677e805c8f84c6ef987692b7b8173d0a9
Nikhil Agarwal <81342764+nikhildagarwal@users.noreply.github.com>
2023-10-21 05:58:51 -0400

    Time: 37 ms (69.11%), Space: 16.1 MB (98.52%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,14 +1,9 @@
+import math
+
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        matrix = []
-        if m == 1  or n == 1:
-            return 1
-        for i in range(m):
-            row  = []
-            for j in range(n):
-                if i == 0 or j == 0:
-                    row.append(1)
-                else:
-                    row.append(matrix[i-1][j] + row[j-1])
-            matrix.append(row)
-        return matrix[m-1][n-1]
+        row = m + n - 2
+        term = m - 1
+        num = math.factorial(row)
+        de = math.factorial(term) * math.factorial(row-term)
+        return int(num/de)
